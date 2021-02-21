3d55f3d1a8f847fd97ae608f3aebf3908f2e5cc9
pophubup <Yohoo0987@gmail.com>
2021-02-21 12:14:31 +0800

    Add refresh token function

--- WebAPI_FireBase_AzureCloud/Controllers/LoginController.cs
+++ WebAPI_FireBase_AzureCloud/Controllers/LoginController.cs
@@ -8,13 +8,14 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI_FireBase_AzureCloud.Models;
 
 namespace WebAPI_FireBase_AzureCloud.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class LoginController : ControllerBase
     {
@@ -41,18 +42,29 @@ namespace WebAPI_FireBase_AzureCloud.Controllers
                 response = Ok(new
                 {
                     token = tokenString,
-                    userDetails = user,
+                    
                 });
             }
             return response;
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult RefreshToken([FromForm] string token)
+        {
+            string mytoken = GetPrincipalFromExpiredToken(token);
+            return Ok(mytoken);
 
+        }
         User AuthenticateUser(User loginCredentials)
         {
             User user = appUsers.SingleOrDefault(x => x.UserName == loginCredentials.UserName && x.Password == loginCredentials.Password);
             return user;
         }
-
+        User FindUserbyName (string fullName)
+        {
+            User user = appUsers.SingleOrDefault(x => x.FullName == fullName);
+            return user;
+        }
         string GenerateJWTToken(User userInfo)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
@@ -70,10 +82,61 @@ namespace WebAPI_FireBase_AzureCloud.Controllers
                 issuer: _config["Jwt:Issuer"],
                 audience: _config["Jwt:Audience"],
                 claims: claims,
-                expires: DateTime.Now.AddMinutes(30),
+                expires: DateTime.Now.Date.AddSeconds(10),
                 signingCredentials: credentials
             );
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
+
+        string GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = _config["Jwt:Issuer"],
+                ValidAudience = _config["Jwt:Audience"],
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"])),
+                ValidateLifetime = false //here we are saying that we don't care about the token's expiration date
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || 
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase) )
+                throw new SecurityTokenException("Invalid token");
+           ;
+            var fullName = principal.Claims.FirstOrDefault(x => x.Type == "fullName").Value;
+            var target = FindUserbyName(fullName);
+            var newJwtToken = RefreshToken(target);
+            
+            return newJwtToken;
+        }
+        string RefreshToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim("fullName", userInfo.FullName.ToString()),
+                new Claim("role",userInfo.UserRole),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var jwt = new JwtSecurityToken(
+               issuer: _config["Jwt:Issuer"],
+               audience: _config["Jwt:Audience"],
+               claims: claims,
+               expires: DateTime.Now.Date.AddDays(1),
+               signingCredentials: credentials
+           );
+            return new JwtSecurityTokenHandler().WriteToken(jwt); //the method is called WriteToken but returns a string
+        }
+     
     }
 }
